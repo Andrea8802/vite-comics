e7b7fe4d58c74d5fab2ff1f59e39b9a7b6c1060f
Andrea8802 <andreabonaventura802@gmail.com>
2022-12-12 17:24:52 +0100

    Aggiunti gli item in modo dinamico alla blue bar

--- src/components/BlueBar.vue
+++ src/components/BlueBar.vue
@@ -3,7 +3,32 @@ export default {
     name: "BlueBar",
     data() {
         return {
+            blueBarContent: [
+                {
+                    name: "DIGITAL COMICS",
+                    image: "buy-comics-digital-comics.png"
+                },
 
+                {
+                    name: "DC MERCHANDISE",
+                    image: "buy-comics-merchandise.png"
+                },
+
+                {
+                    name: "SUSCRIPTION",
+                    image: "buy-comics-subscriptions.png"
+                },
+
+                {
+                    name: "COMIC SHOP LOCATION",
+                    image: "buy-comics-shop-locator.png"
+                },
+
+                {
+                    name: "DC POWER VISA",
+                    image: "buy-dc-power-visa.svg"
+                },
+            ]
         }
     }
 }
@@ -12,24 +37,9 @@ export default {
 <template>
     <section>
         <ul>
-            <li>
-                BBB
-            </li>
-
-            <li>
-                BBB
-            </li>
-
-            <li>
-                BBB
-            </li>
-
-            <li>
-                BBB
-            </li>
-
-            <li>
-                BBB
+            <li v-for="item in blueBarContent">
+                <img :src="'./src/assets/img/' + item.image" alt="">
+                {{ item.name }}
             </li>
         </ul>
     </section>
@@ -41,7 +51,7 @@ section {
     height: 120px;
     width: 100%;
     color: white;
-    padding: 0 150px;
+    padding: 0 220px;
     position: relative;
     z-index: 2;
 }
@@ -51,5 +61,19 @@ ul {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
+}
+
+li {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    font-size: 10px;
+}
+
+ul li img {
+    height: 35%;
+    max-width: 30%;
 }
 </style>
