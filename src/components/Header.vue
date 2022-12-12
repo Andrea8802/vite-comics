f3ba41621cd53d59d3c454516cab60f56b6c5862
Andrea8802 <andreabonaventura802@gmail.com>
2022-12-12 14:54:54 +0100

    Aggiunti file SASS

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+    name: "header",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+
+    <header>
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: red;
+    height: 100px;
+    width: 100px;
+}
+</style>
